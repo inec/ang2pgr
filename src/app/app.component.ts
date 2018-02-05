11b1d29a001dd5b3438e242746ad253292c78c3b
inec <luyege+github@gmail.com>
2018-02-04 23:54:26 -0600

    'test'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   myProp:string="adfasdf sdf";
+
+  myArr=[
+{'title':'my title','desc': 'blog desc'},
+{'title':'my title','desc': 'blog desc'},
+{'title':'my title','desc': 'blog des3c'},
+  ];
   myMethod(){
     this.myProp='diffe';
     console.log('lte');
