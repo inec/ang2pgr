f2b50c9ef6297b3bd159227c215c2dce00a53bb4
inec <luyege+github@gmail.com>
2018-02-05 20:21:07 -0600

    'tt'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import  { AboutComponent } from './about/about.component';
 
 const routes: Routes = [];
 
